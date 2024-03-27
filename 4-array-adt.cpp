00086b29bb166cbdbf03d3e935b04a9fd3dfce29
Arthur Oliveira <arthurolvmorais@brq.com>
2024-03-27 15:59:07 -0300

    refactor(4-array-adt): change functions and struct to a c++ class and methods

--- 4-array-adt.cpp
+++ 4-array-adt.cpp
@@ -2,93 +2,248 @@
 
 using namespace std;
 
-struct Array {
-  int A[10];
+class Array {
+private:
+  int *A;
   int size;
   int length;
+  void swap(int *x, int *y);
+
+public:
+  Array() {
+    size = 10;
+    length = 0;
+    A = new int[size];
+  }
+
+  Array(int sz) {
+    size = sz;
+    length = 0;
+    A = new int[size];
+  }
+
+  ~Array() { delete[] A; }
+
+  /**
+   * Display all Array members based on its length.
+   */
+  void display();
+
+  /**
+   * Add new element at the end (tail) of the Array.
+   */
+  void add(int value);
+
+  /**
+   * Insert new element into specific index of the Array and shift all other
+   * elements if they exists.
+   */
+  void insert(int index, int value);
+
+  /**
+   * Insert new element into sorted Array.
+   */
+  void insertSort(int value);
+
+  /**
+   * Remove element based on its index and return it, also shift all the
+   * remaning elements.
+   */
+  int remove(int index);
+
+  /**
+   * Perform a linear search (O(n)) in Array. Also it's possible to:
+   * - Transposition: It will swap the element searched with the previous
+   * one in order to increase the search time if the same value is searched
+   * again.
+   *
+   * - Move to Head: It will swap the element searched with the first
+   * element of the Array in order to increase the search time if the same value
+   * is searched again.
+   */
+  int linearSearch(int key, bool transposition = false,
+                   bool moveToHead = false);
+
+  /**
+   * Perform a binary search (O(log n)) in Array.
+   */
+  int binarySearch(int key);
+
+  /**
+   * Simply returns the element at the given index.
+   */
+  int get(int index);
+
+  /**
+   * Set the value of the element at the given index.
+   */
+  void set(int index, int value);
+
+  /**
+   * Return the max (greatest) element.
+   */
+  int max();
+
+  /**
+   * Return the min (smallest) element.
+   */
+  int min();
+
+  /**
+   * Return the sum of all elements.
+   */
+  int sum();
+
+  /**
+   * Return the average of all elements.
+   */
+  float avg();
+
+  /**
+   * Reverse the elements. If inPlace is true, it will change the original
+   * Array, otherwise it will create a new Array.
+   */
+  void reverse(bool inPlace);
+
+  /**
+   * Remove first element, shift all other elements to the left and add a 0 to
+   * last space.
+   */
+  int leftShift();
+
+  /**
+   * Remove last element, shift all other elements to the right and add a 0 to
+   * first space.
+   */
+  int rightShift();
+
+  /**
+   * Rotate the Array to the left or to the right.
+   */
+  void rotate(bool right = false);
+
+  /**
+   * Check if the Array is sorted.
+   */
+  bool isSorted();
+
+  /**
+   * Rearrange the array moving all negative elements to left and positives to
+   * right.
+   */
+  void rearrange();
+
+  /**
+   * Merge two sorted arrays into a new array.
+   */
+  Array *merge(Array arr);
+
+  /**
+   * Return the union (same elements in both) of two unsorted arrays (O(n^2)).
+   */
+  Array *unionUnsorted(Array arr);
+
+  /**
+   * Return the union (same elements in both) of two sorted arrays (O(n)).
+   */
+  Array *unionSorted(Array arr);
+
+  /**
+   * Return the intersection (different elements) of two unsorted arrays
+   * (O(n^2)).
+   */
+  Array *intersectionUnsorted(Array arr);
+
+  /**
+   * Return the intersection (different elements) of two sorted arrays (O(n)).
+   */
+  Array *intersectionSorted(Array arr);
 };
 
-void Display(Array *arr) {
-  for (int i = 0; i < arr->length; i++) {
-    cout << arr->A[i] << " ";
-  }
-
-  cout << endl;
-}
-
-void Add(Array *arr, int value) {
-  if (arr->length == arr->size) {
-    printf("No enough space in array\n");
-    return;
-  }
-
-  arr->A[arr->length] = value;
-  arr->length++;
-}
-
-void Insert(Array *arr, int index, int value) {
-  if (index >= arr->size || index < 0 || index > arr->length) {
-    printf("Given index %d is not valid\n", index);
-    return;
-  }
-
-  for (int i = arr->length; i > index; i--) {
-    arr->A[i] = arr->A[i - 1];
-  }
-
-  arr->A[index] = value;
-  arr->length++;
-}
-
-void InsertSorted(Array *arr, int value) {
-  if (arr->length < arr->size) {
-    int i = arr->length - 1;
-
-    while (arr->A[i] > value) {
-      arr->A[i + 1] = arr->A[i];
-      i--;
-    }
-
-    arr->A[i + 1] = value;
-    arr->length++;
-  }
-}
-
-int Delete(Array *arr, int index) {
-  if (arr->length == 0 || index >= arr->length) {
-    printf("Can't delete \n");
-    return -1;
-  }
-
-  int x = arr->A[index];
-
-  arr->length--;
-
-  for (int i = index; i < arr->length; i++) {
-    arr->A[i] = arr->A[i + 1];
-  }
-
-  return x;
-}
-
-void swap(int *x, int *y) {
+void Array::swap(int *x, int *y) {
   int temp = *x;
   *x = *y;
   *y = temp;
 }
 
-int LinearSearch(Array *arr, int key, bool useTransposition = false,
-                 bool useMoveToHead = false) {
+void Array::display() {
+  for (int i = 0; i < this->length; i++) {
+    cout << this->A[i] << " ";
+  }
 
-  for (int i = 0; i < arr->length; i++) {
-    if (arr->A[i] == key) {
-      if (useTransposition) {
-        swap(&arr->A[i], &arr->A[i - 1]);
+  cout << endl;
+}
+
+void Array::add(int value) {
+  if (this->length == this->size) {
+    cout << "No enough space in array" << endl;
+    return;
+  }
+
+  this->A[this->length] = value;
+  this->length++;
+}
+
+void Array::insert(int index, int value) {
+  if (index >= this->size || index < 0 || index > this->length) {
+    cout << "Given index " << index << " is not valid." << endl;
+    return;
+  }
+
+  for (int i = this->length; i > index; i--) {
+    this->A[i] = this->A[i - 1];
+  }
+
+  this->A[index] = value;
+  this->length++;
+}
+
+void Array::insertSort(int value) {
+  if (this->isSorted() == false) {
+    cout << "Array is not sorted" << endl;
+    return;
+  }
+
+  if (this->length < this->size) {
+    int i = this->length - 1;
+
+    while (this->A[i] > value) {
+      this->A[i + 1] = this->A[i];
+      i--;
+    }
+
+    this->A[i + 1] = value;
+    this->length++;
+  }
+}
+
+int Array::remove(int index) {
+  if (this->length == 0 || index >= this->length) {
+    cout << "Can't remove, index out of range" << endl;
+    return -1;
+  }
+
+  int x = this->A[index];
+
+  this->length--;
+
+  for (int i = index; i < this->length; i++) {
+    this->A[i] = this->A[i + 1];
+  }
+
+  return x;
+}
+
+int Array::linearSearch(int key, bool transposition, bool moveToHead) {
+  for (int i = 0; i < this->length; i++) {
+    if (this->A[i] == key) {
+      if (transposition) {
+        this->swap(&this->A[i], &this->A[i - 1]);
         return i - 1;
       }
 
-      if (useMoveToHead) {
-        swap(&arr->A[i], &arr->A[0]);
+      if (moveToHead) {
+        this->swap(&this->A[i], &this->A[0]);
         return 0;
       }
 
@@ -99,17 +254,22 @@ int LinearSearch(Array *arr, int key, bool useTransposition = false,
   return -1;
 }
 
-int BinarySearch(Array *arr, int key) {
+int Array::binarySearch(int key) {
+  if (this->isSorted() == false) {
+    cout << "Array is not sorted" << endl;
+    return -1;
+  }
+
   int low = 0;
-  int high = arr->length;
-  int mid = arr->length / 2;
+  int high = this->length;
+  int mid = this->length / 2;
 
   while (low <= high) {
-    if (arr->A[mid] == key) {
+    if (this->A[mid] == key) {
       return mid;
     }
 
-    if (arr->A[mid] > key) {
+    if (this->A[mid] > key) {
       high = mid - 1;
     } else {
       low = mid + 1;
@@ -121,176 +281,179 @@ int BinarySearch(Array *arr, int key) {
   return -1;
 }
 
-int Get(Array arr, int index) {
-  if (index >= 0 && index < arr.length) {
-    return arr.A[index];
+int Array::get(int index) {
+  if (index >= 0 && index < this->length) {
+    return this->A[index];
   }
   return -1;
 }
 
-void Set(Array *arr, int index, int value) {
-  if (index >= 0 && index < arr->length) {
-    arr->A[index] = value;
+void Array::set(int index, int value) {
+  if (index >= 0 && index < this->length) {
+    this->A[index] = value;
   }
 }
 
-int Max(Array arr) {
-  int max = arr.A[0];
+int Array::max() {
+  int max = this->A[0];
 
-  for (int i = 1; i < arr.length; i++) {
-    if (max < arr.A[i]) {
-      max = arr.A[i];
+  for (int i = 1; i < this->length; i++) {
+    if (max < this->A[i]) {
+      max = this->A[i];
     }
   }
 
   return max;
 }
 
-int Min(Array arr) {
-  int min = arr.A[0];
+int Array::min() {
+  int min = this->A[0];
 
-  for (int i = 1; i < arr.length; i++) {
-    if (min > arr.A[i]) {
-      min = arr.A[i];
+  for (int i = 1; i < this->length; i++) {
+    if (min > this->A[i]) {
+      min = this->A[i];
     }
   }
 
   return min;
 }
 
-int Sum(Array arr) {
+int Array::sum() {
   int sum = 0;
 
-  for (int i = 0; i < arr.length; i++) {
-    sum += arr.A[i];
+  for (int i = 0; i < this->length; i++) {
+    sum += this->A[i];
   }
 
   return sum;
 }
 
-float Avg(Array arr) {
-  int sum = (float)Sum(arr);
-  return sum / (float)arr.length;
+float Array::avg() {
+  int sum = (float)this->sum();
+  return sum / (float)this->length;
 }
 
-void Reverse(Array *arr, bool inPlace = false) {
+void Array::reverse(bool inPlace = false) {
   if (inPlace) {
-    for (int i = 0; i < arr->length / 2; i++) {
-      int x = arr->A[i];
-      arr->A[i] = arr->A[arr->length - 1 - i];
-      arr->A[arr->length - 1 - i] = x;
+    for (int i = 0; i < this->length / 2; i++) {
+      int x = this->A[i];
+      this->A[i] = this->A[this->length - 1 - i];
+      this->A[this->length - 1 - i] = x;
     }
     return;
   }
 
-  int B[arr->length];
+  int B[this->length];
 
-  for (int i = arr->length - 1, j = 0; i >= 0; i--, j++) {
-    B[j] = arr->A[i];
+  for (int i = this->length - 1, j = 0; i >= 0; i--, j++) {
+    B[j] = this->A[i];
   }
 
-  for (int i = 0; i < arr->length; i++) {
-    arr->A[i] = B[i];
+  for (int i = 0; i < this->length; i++) {
+    this->A[i] = B[i];
   }
 }
 
-int LeftShift(Array *arr) {
-  int x = arr->A[0];
-  for (int i = 0; i < arr->length; i++) {
-    arr->A[i] = arr->A[i + 1];
+int Array::leftShift() {
+  int x = this->A[0];
+
+  for (int i = 0; i < this->length; i++) {
+    this->A[i] = this->A[i + 1];
   }
-  arr->A[arr->length - 1] = 0;
+
+  this->A[this->length - 1] = 0;
+
   return x;
 }
 
-int RightShift(Array *arr) {
-  int x = arr->A[arr->length - 1];
-  for (int i = arr->length - 1; i >= 0; i--) {
-    arr->A[i] = arr->A[i - 1];
+int Array::rightShift() {
+  int x = this->A[this->length - 1];
+
+  for (int i = this->length - 1; i >= 0; i--) {
+    this->A[i] = this->A[i - 1];
   }
-  arr->A[0] = 0;
+
+  this->A[0] = 0;
+
   return x;
 }
 
-void Rotate(Array *arr, bool right = false) {
+void Array::rotate(bool right) {
   int x;
 
   if (right) {
-    x = RightShift(arr);
-    arr->A[0] = x;
+    x = this->rightShift();
+    this->A[0] = x;
     return;
   }
 
-  x = LeftShift(arr);
-  arr->A[arr->length - 1] = x;
+  x = this->leftShift();
+  this->A[this->length - 1] = x;
 }
 
-bool IsSorted(Array arr) {
-  for (int i = 0; i < arr.length - 1; i++) {
-    if (arr.A[i] > arr.A[i + 1]) {
+bool Array::isSorted() {
+  for (int i = 0; i < this->length - 1; i++) {
+    if (this->A[i] > this->A[i + 1]) {
       return false;
     }
   }
   return true;
 }
 
-void MoveNegativesForLeftSide(Array *arr) {
+void Array::rearrange() {
   int i = 0;
-  int j = arr->length;
+  int j = this->length;
 
   while (i < j) {
-    while (arr->A[i] < 0) {
+    while (this->A[i] < 0) {
       i++;
     }
-    while (arr->A[j] > 0) {
+    while (this->A[j] > 0) {
       j--;
     }
 
     if (i < j) {
-      swap(&arr->A[i], &arr->A[j]);
+      this->swap(&this->A[i], &this->A[j]);
     }
   }
 }
 
-Array *Merge(Array m, Array n) {
-  Array *result = new Array;
-  result->length = m.length + n.length;
-  result->size = m.size + n.size;
+Array *Array::merge(Array arr) {
+  Array *result = new Array(this->length + arr.length);
 
   int i = 0, j = 0, k = 0;
 
-  while (i < m.length && j < n.length) {
-    if (m.A[i] < n.A[j]) {
-      result->A[k++] = m.A[i++];
+  while (i < this->length && j < arr.length) {
+    if (this->A[i] < arr.A[j]) {
+      result->A[k++] = this->A[i++];
     } else {
-      result->A[k++] = n.A[j++];
+      result->A[k++] = arr.A[j++];
     }
   }
 
-  for (; i < m.length; i++) {
-    result->A[k++] = m.A[i];
+  for (; i < this->length; i++) {
+    result->A[k++] = this->A[i];
   }
 
-  for (; j < n.length; j++) {
-    result->A[k++] = n.A[j];
+  for (; j < arr.length; j++) {
+    result->A[k++] = arr.A[j];
   }
 
   return result;
 }
 
-Array *UnionUnsorted(Array m, Array n) {
-  Array *result = new Array;
-  result->length = m.length;
+Array *Array::unionUnsorted(Array arr) {
+  Array *result = new Array(this->length + arr.length);
 
-  int i;
+  int i, j;
 
-  for (i = 0; i < m.length; i++) {
-    result->A[i] = m.A[i];
+  for (i = 0; i < this->length; i++) {
+    result->A[i] = this->A[i];
   }
 
-  for (int j = 0; j < n.length; j++) {
-    if (LinearSearch(&m, n.A[j]) == -1) {
-      result->A[i] = n.A[j];
+  for (j = 0; j < arr.length; j++) {
+    if (this->linearSearch(arr.A[j]) == -1) {
+      result->A[i] = arr.A[j];
       result->length++;
       i++;
     }
@@ -299,59 +462,49 @@ Array *UnionUnsorted(Array m, Array n) {
   return result;
 }
 
-Array *UnionSorted(Array m, Array n) {
-  Array *result = new Array;
-  result->length = 0;
+Array *Array::unionSorted(Array arr) {
+  Array *result = new Array(this->length + arr.length);
 
   int i = 0, j = 0, k = 0;
 
-  while (i < m.length && j < n.length) {
+  while (i < this->length && j < arr.length) {
     result->length++;
 
-    if (m.A[i] == n.A[j]) {
-      result->A[k++] = m.A[i++];
+    if (this->A[i] == arr.A[j]) {
+      result->A[k++] = this->A[i++];
       j++;
       continue;
     }
 
-    if (m.A[i] > n.A[j]) {
-      result->A[k++] = n.A[j++];
+    if (this->A[i] > arr.A[j]) {
+      result->A[k++] = arr.A[j++];
       continue;
     }
 
-    result->A[k++] = m.A[i++];
+    result->A[k++] = this->A[i++];
   }
 
-  for (; i < m.length; i++) {
+  for (; i < this->length; i++) {
     result->length++;
-    result->A[k++] = m.A[i];
+    result->A[k++] = this->A[i];
   }
 
-  for (; j < m.length; j++) {
+  for (; j < this->length; j++) {
     result->length++;
-    result->A[k++] = n.A[j];
+    result->A[k++] = arr.A[j];
   }
 
   return result;
 }
 
-Array *IntersectionUnsorted(Array m, Array n) {
-  Array *result = new Array;
-  result->length = 0;
+Array *Array::intersectionUnsorted(Array arr) {
+  Array *result = new Array(this->length + arr.length);
 
   int k = 0;
 
-  Array largestArray = m;
-  Array smallestArray = n;
-
-  if (m.length < n.length) {
-    largestArray = n;
-    smallestArray = m;
-  }
-
-  for (int i = 0; i < largestArray.length; i++) {
-    if (LinearSearch(&smallestArray, largestArray.A[i]) != -1) {
-      result->A[k++] = largestArray.A[i];
+  for (int i = 0; i < this->length; i++) {
+    if (this->linearSearch(arr.A[i]) != -1) {
+      result->A[k++] = arr.A[i];
       result->length++;
     }
   }
@@ -359,20 +512,20 @@ Array *IntersectionUnsorted(Array m, Array n) {
   return result;
 }
 
-Array *IntersectionSorted(Array m, Array n) {
-  Array *result = new Array;
+Array *Array::intersectionSorted(Array arr) {
+  Array *result = new Array(this->length + arr.length);
 
   int i = 0, j = 0, k = 0;
 
-  while (i < m.length && j < n.length) {
-    if (m.A[i] == n.A[j]) {
-      result->A[k++] = m.A[i++];
+  while (i < this->length && j < arr.length) {
+    if (this->A[i] == arr.A[j]) {
+      result->A[k++] = this->A[i++];
       result->length++;
       j++;
       continue;
     }
 
-    if (m.A[i] < n.A[j]) {
+    if (this->A[i] < arr.A[j]) {
       i++;
       continue;
     }
@@ -383,105 +536,4 @@ Array *IntersectionSorted(Array m, Array n) {
   return result;
 }
 
-int main() {
-  // Array arr = {{2, 3, 4, 5, 6}, 10, 5};
-
-  // Display(&arr);
-  // Add(&arr, 7);
-  // Add(&arr, 8);
-  // Add(&arr, 9);
-  // Display(&arr);
-  // Insert(&arr, 0, 1);
-  // Display(&arr);
-  // int deleted = Delete(&arr, 2);
-  // printf("Element deleted: %d \n", deleted);
-  // Display(&arr);
-  // int index = LinearSearch(&arr, 6, true);
-  // printf("Element found at index: %d \n", index);
-  // Display(&arr);
-
-  // int binarySearchResult = BinarySearch(&arr, 9);
-  // printf("Binary search element found at index: %d \n", binarySearchResult);
-
-  // printf("Max element is %d\n", Max(arr));
-  // printf("Min element is %d\n", Min(arr));
-  // printf("Sum of elements is %d\n", Sum(arr));
-  // printf("Avg of elements is %f\n", Avg(arr));
-
-  // Reverse(&arr);
-  // printf("Reversed array: ");
-  // Display(&arr);
-
-  // Reverse(&arr, true);
-  // printf("Reversed array in place: ");
-  // Display(&arr);
-
-  // LeftShift(&arr);
-  // printf("Array left shifted: ");
-  // Display(&arr);
-
-  // RightShift(&arr);
-  // printf("Array right shifted: ");
-  // Display(&arr);
-
-  // Rotate(&arr, true);
-  // printf("Array left rotated: ");
-  // Display(&arr);
-
-  // Rotate(&arr, true);
-  // printf("Array right rotated: ");
-  // Display(&arr);
-
-  // InsertSorted(&arr, 3);
-  // printf("Number 3 sorted inserted: ");
-  // Display(&arr);
-  // printf("Array is sorted? %d\n", IsSorted(arr));
-
-  // Array arr2 = {{-2, 3, 14, 5, -6, 10, -5, 22, 1, -8}, 10, 10};
-  // printf("Negatives moved for the left side: ");
-  // MoveNegativesForLeftSide(&arr2);
-  // Display(&arr2);
-
-  // Array arr1 = {{2, 5, 8, 10, 16}, 10, 5};
-  // Array arr2 = {{1, 3, 14, 21, 23}, 10, 5};
-
-  // printf("-------------------\n");
-  // printf("arr1: ");
-  // Display(&arr1);
-  // printf("arr2: ");
-  // Display(&arr2);
-
-  // Array *mergedArr = Merge(arr1, arr2);
-  // printf("Arrays merged: ");
-  // Display(mergedArr);
-
-  // Array arr1 = {{2, 8, 10, 16, 5}, 10, 5};
-  // Array arr2 = {{2, 21, 3, 10, 8}, 10, 5};
-
-  // Array *unionArr = UnionUnsorted(arr1, arr2);
-  // printf("Arrays union: ");
-  // Display(unionArr);
-
-  // Array arr1 = {{2, 5, 8, 10, 16}, 10, 5};
-  // Array arr2 = {{1, 3, 8, 16, 23}, 10, 5};
-
-  // Array *unionSortedArr = UnionSorted(arr1, arr2);
-  // printf("Arrays union: ");
-  // Display(unionSortedArr);
-
-  // Array arr1 = {{2, 8, 10, 16, 5}, 10, 5};
-  // Array arr2 = {{2, 21, 3, 10, 8}, 10, 5};
-
-  // Array *intersectionUnsortedArr = IntersectionUnsorted(arr1, arr2);
-  // printf("Arrays intersection: ");
-  // Display(intersectionUnsortedArr);
-
-  Array arr1 = {{2, 5, 8, 10, 16}, 10, 5};
-  Array arr2 = {{1, 3, 8, 16, 23}, 10, 5};
-
-  Array *intersectionSortedArr = IntersectionSorted(arr1, arr2);
-  printf("Arrays intersection: ");
-  Display(intersectionSortedArr);
-
-  return 0;
-}
+int main() { return 0; }
